215fe7d5c7cde345fc3dcc585db9dd7745cb6bed
POlbrot <polbrot@hemmersbach.com>
2018-03-09 13:32:23 +0100

    Add custom Exception and NotFoundResponse

---
+++ src/POlbrot/Exceptions/URLNotMatchedException.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace POlbrot\Exceptions;
+
+use Throwable;
+
+/**
+ * Class URLNotMatchedException
+ *
+ * @package POlbrot\Exceptions
+ */
+class URLNotMatchedException extends \Exception
+{
+    /**
+     * URLNotMatchedException constructor.
+     *
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return 'URLNotMatchedException: URL could not be matched to any existing endpoints';
+    }
+}
--- src/POlbrot/HTTP/NotFoundResponse.php
+++ src/POlbrot/HTTP/NotFoundResponse.php
@@ -7,7 +7,7 @@ namespace POlbrot\HTTP;
  *
  * @package POlbrot\HTTP
  */
-class TeapotResponse extends Response
+class NotFoundResponse extends Response
 {
     /**
      * Accepts content that will be turned into the JSON object and adds an appropriate header
@@ -16,9 +16,7 @@ class TeapotResponse extends Response
      */
     public function __construct($content = '')
     {
-        parent::__construct(
-            '<img src="https://orig00.deviantart.net/eaa7/f/2014/050/6/c/error_418_i_m_a_teapot_by_ai_15837-d7789z0.jpg" alt="teapot">'
-        );
-        $this->addHeader('HTTP/1.0 418 Not Found');
+        parent::__construct($content);
+        $this->addHeader('HTTP/1.0 404 Not Found');
     }
 }
