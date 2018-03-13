ff335ded5dff237bf85d15a6576c7ab26adcae99
POlbrot <polbrot@hemmersbach.com>
2018-03-13 14:49:41 +0100

    Divide picking random and setting to distinct setter methods

--- src/POlbrot/Model/UserBuilder.php
+++ src/POlbrot/Model/UserBuilder.php
@@ -3,9 +3,7 @@
 namespace POlbrot\Model;
 
 use POlbrot\DataProvider\DataProviderInterface;
-use POlbrot\DataProvider\JSONDataProvider;
-use POlbrot\Exceptions\InvalidJSONFileException;
-use POlbrot\Exceptions\InvalidJSONPathException;
+use POlbrot\Helpers\Helpers;
 
 /**
  * Class UserBuilder
@@ -18,15 +16,6 @@ class UserBuilder
     private $user;
     private $firstNames;
     private $lastNames;
-
-    /**
-     * @param $arr
-     * @return mixed
-     */
-    private static function pickOneRandom($arr)
-    {
-        return $arr[array_rand($arr)];
-    }
 
     /**
      * @return $this
@@ -69,13 +58,101 @@ class UserBuilder
     }
 
     /**
+     * @return $this
+     */
+    private function setRandomFirstName()
+    {
+        $firstName = Helpers::pickOneRandom($this->firstNames);
+
+        $this->user->setFirstName($firstName);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setRandomLastName()
+    {
+        $lastName = Helpers::pickOneRandom($this->lastNames);
+
+        $this->user->setLastName($lastName);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setUsername()
+    {
+        $username = strtolower($this->user->getFirstName()[0] . $this->user->getLastName());
+
+        $this->user->setUsername($username);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setEmail()
+    {
+        $email = $this->user->getUsername() . '@fmail.com';
+
+        $this->user->setEmail($email);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setRandomGender()
+    {
+        $gender = Helpers::pickOneRandom(['male', 'female']);
+
+        $this->user->setGender($gender);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setRandomPassword()
+    {
+        $password = Helpers::pickRandomAlphanumeric(6, 12);
+
+        $this->user->setPassword($password);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    private function setRandomSalt()
+    {
+        $salt = Helpers::pickRandomAlphanumeric();
+
+        $this->user->setSalt($salt);
+
+        return $this;
+    }
+    /**
      * @return User
      */
     public function getUser()
     {
-        $this->init();
-        $this->user->setFirstName(self::pickOneRandom($this->firstNames));
-        $this->user->setLastName(self::pickOneRandom($this->lastNames));
-        return $this->build();
+        return $this->init()
+            ->setRandomFirstName()
+            ->setRandomLastName()
+            ->setUsername()
+            ->setEmail()
+            ->setRandomGender()
+            ->setRandomPassword()
+            ->setRandomSalt()
+            ->build();
     }
 }
