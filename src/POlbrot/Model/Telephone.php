61880383800ffd8758d350f7b51407ae0a5066bf
POlbrot <polbrot@hemmersbach.com>
2018-03-13 10:39:53 +0100

    Create Telephone class

--- src/POlbrot/Model/Telephone.php
+++ src/POlbrot/Model/Telephone.php
@@ -1,8 +1,44 @@
 <?php
 
+namespace POlbrot\Model;
+
 /**
  * Class Telephone
  */
-class Telephone {
+class Telephone
+{
+    private $type;
+    private $number;
 
+    /**
+     * @return mixed
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    /**
+     * @param mixed $number
+     */
+    public function setNumber($number): void
+    {
+        $this->number = $number;
+    }
 }
