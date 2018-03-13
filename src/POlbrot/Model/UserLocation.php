1009d6a720358fa4d38c4b8762107668f0b3ec48
POlbrot <polbrot@hemmersbach.com>
2018-03-13 10:47:10 +0100

    Create UserLocation class

--- src/POlbrot/Model/UserLocation.php
+++ src/POlbrot/Model/UserLocation.php
@@ -1,8 +1,77 @@
 <?php
 
+namespace POlbrot\Model;
+
 /**
  * Class UserLocation
  */
 class UserLocation {
+    private $country;
+    private $city;
+    private $street;
+    private $zipCode;
 
+    /**
+     * @return mixed
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param mixed $street
+     */
+    public function setStreet($street): void
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipCode(): string
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @param mixed $zipCode
+     */
+    public function setZipCode($zipCode): void
+    {
+        $this->zipCode = $zipCode;
+    }
 }
