e3ac5e1cca6abd28a02d480354bf337711fdff9c
POlbrot <polbrot@hemmersbach.com>
2018-03-13 14:50:41 +0100

    Move random-picking to Helpers

--- src/POlbrot/Helpers/Helpers.php
+++ src/POlbrot/Helpers/Helpers.php
@@ -32,4 +32,31 @@ class Helpers
             throw new InvalidJSONPathException();
         }
     }
+
+    /**
+     * @param $arr
+     * @return mixed
+     */
+    public static function pickOneRandom($arr)
+    {
+        return $arr[array_rand($arr)];
+    }
+
+    /**
+     * @param int $minLength
+     * @param int $maxLength
+     * @return string
+     */
+    public static function pickRandomAlphanumeric(int $minLength = 6, int $maxLength = 30)
+    {
+        $possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $string = '';
+        $stringLength = rand($minLength, $maxLength);
+
+        for ($letterIndex = 0; $letterIndex < $stringLength; $letterIndex++) {
+            $string .= $possibleChars[rand(0, strlen($possibleChars) - 1)];
+        }
+
+        return $string;
+    }
 }
