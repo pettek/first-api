be844b5addcc8f780961d63d09ab47d10f3935da
POlbrot <polbrot@hemmersbach.com>
2018-03-15 10:18:37 +0100

    Add comments to Controller and UserController

--- src/POlbrot/Controller/Controller.php
+++ src/POlbrot/Controller/Controller.php
@@ -4,6 +4,7 @@ namespace POlbrot\Controller;
 
 /**
  * Class Controller
+ * This is the place for methods and properties that will be shared by all the controllers in the future
  *
  * @package POlbrot\Router
  */
--- src/POlbrot/Controller/UserController.php
+++ src/POlbrot/Controller/UserController.php
@@ -15,22 +15,26 @@ class UserController extends Controller
 {
     /**
      * Returns instance of JSONResponse with some fake user data as content
+     * Depends on data from files (via UserBuilderCreator::get) so it may throw exceptions indicating that data file
+     * cannot be read (does not exist or is not valid)
      *
      * @param Request $request
      *
      * @return JsonResponse
      * @throws \POlbrot\Exceptions\InvalidJSONPathException
      * @throws \POlbrot\Exceptions\InvalidTextFilePathException
-     * @throws \Exception
      */
     public function getAction(Request $request): JsonResponse
     {
-
         $users = [];
+
+        // Show one user if not specified in request attribute
         $howManyToGenerate = $request->attributes->get('number') ?? 1;
 
+        // Get instance of builder already filled with data from datafiles
         $builder = UserBuilderCreator::get();
 
+        // Fill $users array with some random data
         for ($i = 0; $i < $howManyToGenerate; $i++) {
             $user = $builder->getUser();
 
@@ -38,27 +42,5 @@ class UserController extends Controller
         }
 
         return new JsonResponse($users);
-    }
-
-    /**
-     * @param Request $request
-     *
-     * @return JsonResponse
-     */
-    public function findAction(Request $request): JsonResponse
-    {
-        $age = $request->attributes->get('age') ?? 'defaultAge';
-        $name = $request->attributes->get('name') ?? 'defaultName';
-
-        $user = [
-            'name' => [
-                'first' => $name,
-                'last' => 'Nazwisko',
-            ],
-            'location' => 'Wroclaw',
-            'age' => $age,
-        ];
-
-        return new JsonResponse($user);
     }
 }
