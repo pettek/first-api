21278aa082f44d5bf16068fcbc7819a772c896ad
POlbrot <polbrot@hemmersbach.com>
2018-03-16 15:29:30 +0100

    Add application property to Controller (and getters/setters)

--- src/POlbrot/Controller/Controller.php
+++ src/POlbrot/Controller/Controller.php
@@ -2,6 +2,8 @@
 
 namespace POlbrot\Controller;
 
+use POlbrot\Application\Application;
+
 /**
  * Class Controller
  * This is the place for methods and properties that will be shared by all the controllers in the future
@@ -10,5 +12,27 @@ namespace POlbrot\Controller;
  */
 class Controller
 {
+    /** @var Application */
+    protected $application;
+
+    /**
+     * @return mixed
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    /**
+     * @param mixed $application
+     * @return Controller
+     */
+    public function setApplication($application): Controller
+    {
+        $this->application = $application;
+
+        return $this;
+    }
+
 
 }
