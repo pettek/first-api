b6f7f7b0c13f59694cc5ab10f726d7664184c03d
POlbrot <polbrot@hemmersbach.com>
2018-03-16 15:29:02 +0100

    Create User Entity

---
+++ src/POlbrot/Entity/User.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace POlbrot\Entity;
+
+use Doctrine\ORM\Mapping\{Id, Entity, Column, GeneratedValue, Table};
+
+/**
+ * @Entity
+ * @Table(name="user")
+ */
+class User
+{
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    public $id;
+
+    /**
+     * @Column()
+     */
+    public $firstName;
+
+    /**
+     * @Column()
+     */
+    public $lastName;
+
+    /**
+     * @Column()
+     */
+    public $username;
+
+    /**
+     * @Column()
+     */
+    public $password;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $firstName
+     */
+    public function setFirstName($firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $lastName
+     */
+    public function setLastName($lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $userName
+     */
+    public function setUsername($userName): void
+    {
+        $this->username = $userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+}
